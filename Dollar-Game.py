2fc33b575dd8a5fbd2bfbe14cd5bac2514e67172
JackMcKechnie <jackmckechnie@sky.com>
2020-05-08 14:44:54 +0200

    Test and print graph

--- Dollar-Game.py
+++ Dollar-Game.py
@@ -1,4 +1,4 @@
-class graph(object):
+class Graph(object):
     def __init__(self,graph_dict = None):
         """Initialise a graph"""
         if graph_dict == None:
@@ -8,7 +8,7 @@ class graph(object):
     def vertices(self):
         """Getter that returns the
         the vertices of the graph"""
-        return list(self.__graph_dict.keys())
+        return list(self.graph_dict.keys())
 
     def edges(self):
         """Getter that returns the
@@ -19,18 +19,18 @@ class graph(object):
         """Adds the given vertex
         by adding a new entry to the
         graph_dict: {vertex:[]}"""
-        if vertex not in self.__graph_dict:
-            self.__graph_dict[vertex] = []
+        if vertex not in self.graph_dict:
+            self.graph_dict[vertex] = []
 
     def add_edge(self, edge):
         """Adds an edge between
         two vertices"""
         edge = set(edge)
         (vertex1, vertex2) = tuple(edge)
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
+        if vertex1 in self.graph_dict:
+            self.graph_dict[vertex1].append(vertex2)
         else:
-            self.__graph_dict[vertex1] = [vertex2]
+            self.graph_dict[vertex1] = [vertex2]
 
     def __generate_edges(self):
         """Static method to
@@ -38,8 +38,8 @@ class graph(object):
         graph. An edge is a set with
         one or two vertices"""
         edges = []
-        for vertex in self.__graph_dict:
-            for neighbor in self.__graph_dict[vertex]:
+        for vertex in self.graph_dict:
+            for neighbor in self.graph_dict[vertex]:
                 if {neighbor, vertex} not in edges:
                     edges.append({vertex, neighbor})
         return edges
@@ -52,6 +52,22 @@ class graph(object):
         for edge in self.__generate_edges():
             result += str(edge) + " "
         return result
+
+if __name__ == "__main__":
+
+    g = {"a": ["d"],
+         "b": ["c"],
+         "c": ["b", "c", "d", "e"],
+         "d": ["a", "c"],
+         "e": ["c"],
+         "f": []
+         }
+    graph = Graph(g)
+
+    print("Graph:")
+    print(str(g))
+
+
 
 
 
